06f87bc7af3da0539fa9ae55e19c7bce3f5a2bd3
BambiRocks <bambi_rocks@outlook.com>
2023-02-10 13:28:59 +0100

    Añadida tabla en panel_profesor.php

--- panel_profesor.php
+++ panel_profesor.php
@@ -13,9 +13,31 @@ if(isset($_SESSION['usuario'])){
             <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
             <title>Panel profesor</title>
         </head>
-        <body>";
+        <body>
+            <tr>
+                <th>Correo</th>
+                <th>Descripcion</th>
+                <th>Opciones</th>
+            </tr>";
 
-        
+        /*En un futuro, esto podría llamar a una función que estuviera en una clase profesor? en lugar
+        llamar directamente a Conexion.php */
+
+        $alumnos = listarPorCampo('usuario', 'WHERE tipo = :tipo', array('tipo' => 1)); 
+
+        for($i = 0; $i < count($alumnos); $i++){
+
+            echo "          <tr>
+            <td>" . $alumnos[$i]->getCorreo() . "</td>
+            <td>" . $alumnos[$i]->getDescripcion() . "</td>
+            <td>
+                <ul>
+                    <li>Actualizar</li>
+                    <li>Eliminar</li>
+                </ul>
+            </td>
+            </tr>\n";
+        }
             
         echo "</body>
         </html>";
